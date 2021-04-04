eae4c7dc486c27b083e705ca9acb32f158d81cbd
Marek Piotrowski <marek.piotrowski@Mareks-MacBook-Pro.local>
2021-04-04 22:55:03 +0200

    add frameworks

---
+++ Faith/main/MainFaithViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Faith
+//
+//  Created by Marek Piotrowski on 04/04/2021.
+//
+
+import UIKit
+import SnapKit
+
+class MainFaithViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        view.snp_makeConstraints { make in
+            
+        }
+    }
+
+}
+
